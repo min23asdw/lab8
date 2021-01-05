bf6d0c236a20db02f473d58beda1f7630475dace
min23asdw <min23asdw@outlook.com>
2021-01-05 15:29:48 +0700

    D

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -2,32 +2,49 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+	double loan,rate,payment;
+	double prevbalance,interest,total,newbalance;
+	int year=1;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> payment;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
-	cout << setw(13) << left << "EndOfYear#"; 
-	cout << setw(13) << left << "PrevBalance"; 
-	cout << setw(13) << left << "Interest"; 
+	cout << setw(13) << left << "EndOfYear#";
+	cout << setw(13) << left << "PrevBalance";
+	cout << setw(13) << left << "Interest";
 	cout << setw(13) << left << "Total";
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
+
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	while (true){
+		if(year==1){prevbalance=loan;}
+
+	interest = prevbalance*rate/100.00;
+	total=prevbalance+interest;
+		if(total < payment){payment=total;}
+		newbalance=total-payment;
+
+	cout << fixed << setprecision(2);
+	cout << setw(13) << left << year;
+	cout << setw(13) << left << prevbalance;
+	cout << setw(13) << left << interest;
+	cout << setw(13) << left << total;
+	cout << setw(13) << left << payment;
+	cout << setw(13) << left << newbalance;
+	cout << "\n";
+	if(newbalance==0){break;}
+	prevbalance=newbalance;
+	year++;
+  }
 	return 0;
 }
