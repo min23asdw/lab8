1d1be1fce6b63a5cddd7021d9540258d34b502e5
min23asdw <min23asdw@outlook.com>
2021-01-05 15:40:25 +0700

    D

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,24 +1,22 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int N,int M) {
+if(M>0&&N>0){
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cout << "O";
+		}
+		cout<< "\n";
+	}
+}else{cout << "Invalid input";}
+}
 
 int main(){
-	
-	printO(2,2);
-	cout << "\n";
-	
-	printO(3,5);
-	cout << "\n";	
-	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
+
+
 	printO(7,-1);
-	cout << "\n";	
-	
+	cout << "\n";
+
 	return 0;
 }
